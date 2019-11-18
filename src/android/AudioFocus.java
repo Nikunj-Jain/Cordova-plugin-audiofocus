333f0b4274f8adc50bd8f7969e21f083ceb89005
Nikunj Jain <nikunj.jain@myrealdata.net>
2019-11-18 19:05:59 +0530

    Improvements and optimizations

--- src/android/AudioFocus.java
+++ src/android/AudioFocus.java
@@ -11,10 +11,11 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
 public class AudioFocus extends CordovaPlugin {
 
-    private final static String TAG = "AudioFocus.java";
+    private final static String TAG = "AudioFocus";
 
     private static final int MODE_IN_COMMUNICATION = 100;
     private static final int MODE_NORMAL = 101;
@@ -26,26 +27,30 @@ public class AudioFocus extends CordovaPlugin {
 
     @Override
     protected void pluginInitialize() {
-        mAudioManager = (AudioManager)this.cordova.getActivity().getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        mFocusChangeListener = new onFocusChangeListener();
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                mAudioManager = (AudioManager)cordova.getActivity().getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
+                mFocusChangeListener = new onFocusChangeListener();
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 
-            AudioAttributes mAudioAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
-                    .build();
+                    AudioAttributes mAudioAttributes = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+                            .build();
 
-            mAudioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
-                    .setAudioAttributes(mAudioAttributes)
-                    .setAcceptsDelayedFocusGain(true)
-                    .setOnAudioFocusChangeListener(mFocusChangeListener)
-                    .build();
-        }
+                    mAudioFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+                            .setAudioAttributes(mAudioAttributes)
+                            .setAcceptsDelayedFocusGain(true)
+                            .setOnAudioFocusChangeListener(mFocusChangeListener)
+                            .build();
+                }
+            }
+        });
     }
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) {
 
         if (mAudioManager == null) {
             String errorMessage = "Audio manager out of memory";
@@ -54,76 +59,90 @@ public class AudioFocus extends CordovaPlugin {
             return true;
         }
 
-        int audioMode = 0;
-        try { audioMode = args.getInt(0); }
-        catch (Exception e) {}
-        setAudioMode(audioMode);
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                int audioMode = 0;
+                try { audioMode = args.getInt(0); }
+                catch (JSONException e) {}
+                setAudioMode(audioMode);
+            }
+        });
 
         if (action.equals("requestFocus")) {
-            if (this.requestFocus()) {
-                callbackContext.success("");
-            } else {
-                callbackContext.error("");
-            }
+            requestFocus(callbackContext);
             return true;
         } else if (action.equals("dumpFocus")) {
-            if (this.dumpFocus()) {
-                callbackContext.success("");
-            } else {
-                callbackContext.error("");
-            }
+            dumpFocus(callbackContext);
             return true;
         }
 
         return false;
     }
 
-    private boolean requestFocus() {
+    private void requestFocus(final CallbackContext callbackContext) {
 
-        int result;
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            result = mAudioManager.requestAudioFocus(mAudioFocusRequest);
-        } else {
-            result = mAudioManager.requestAudioFocus(mFocusChangeListener, AudioManager.STREAM_VOICE_CALL, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-        }
+                int result;
 
-        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-            String str = "Successfully received audio focus.";
-            Log.i(TAG, str);
-            return true;
-        } else {
-            String str = "Getting audio focus failed.";
-            Log.i(TAG, str);
-            return false;
-        }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    result = mAudioManager.requestAudioFocus(mAudioFocusRequest);
+                } else {
+                    result = mAudioManager.requestAudioFocus(mFocusChangeListener, AudioManager.STREAM_VOICE_CALL, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                }
+
+                if (callbackContext == null) {
+                    return;
+                }
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    String str = "Successfully received audio focus.";
+                    Log.i(TAG, str);
+                    callbackContext.success(str);
+                } else {
+                    String str = "Getting audio focus failed.";
+                    Log.e(TAG, str);
+                    callbackContext.error(str);
+                }
+            }
+        });
 
     }
 
-    private boolean dumpFocus() {
+    private void dumpFocus(final CallbackContext callbackContext) {
 
-        int result;
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            result = mAudioManager.abandonAudioFocusRequest(mAudioFocusRequest);
-        } else {
-            result = mAudioManager.abandonAudioFocus(mFocusChangeListener);
-        }
+                int result;
 
-        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-            String str = "Abandoned audio focus successfully.";
-            Log.i(TAG, str);
-            return true;
-        } else {
-            String str = "Abandoning audio focus failed.";
-            Log.i(TAG, str);
-            return false;
-        }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    result = mAudioManager.abandonAudioFocusRequest(mAudioFocusRequest);
+                } else {
+                    result = mAudioManager.abandonAudioFocus(mFocusChangeListener);
+                }
+
+                if (callbackContext == null) {
+                    return;
+                }
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    String str = "Abandoned audio focus successfully.";
+                    Log.i(TAG, str);
+                    callbackContext.success(str);
+                } else {
+                    String str = "Abandoning audio focus failed.";
+                    Log.e(TAG, str);
+                    callbackContext.error(str);
+                }
+            }
+        });
     }
 
     @Override
     public void onDestroy() {
-        dumpFocus();
+        dumpFocus(null);
     }
 
     private void setAudioMode(int audioMode) {
